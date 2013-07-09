687c55d0bff95d91dd2ca1b82fde3b682df24d9d
driodmaster92 <adityasureka92@gmail.com>
2013-07-09 20:39:29 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,18 @@ add_lunch_combo illusion_jflteusc-userdebug
 add_lunch_combo illusion_jfltevzw-userdebug
 add_lunch_combo illusion_mako-userdebug
 add_lunch_combo illusion_ville-userdebug
-
+add_lunch_combo illusion_galaxysmtd-userdebug
+add_lunch_combo illusion_i9100-userdebug
+add_lunch_combo illusion_m7ul-userdebug
+add_lunch_combo illusion_n7000-userdebug
+add_lunch_combo illusion_n7100-userdebug
+add_lunch_combo illusion_m7att-userdebug
+add_lunch_combo illusion_m7tmo-userdebug
+add_lunch_combo illusion_m7spr-userdebug
+add_lunch_combo illusion_crespo-userdebug
+add_lunch_combo illusion_t0lte-userdebug
+add_lunch_combo illusion_t0lteatt-userdebug
+add_lunch_combo illusion_t0ltetmo-userdebug
+add_lunch_combo illusion_maguro-userdebug
+add_lunch_combo illusion_toro-userdebug
+add_lunch_combo illusion_toroplus-userdebug
