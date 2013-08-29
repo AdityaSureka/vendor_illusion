e4fd7468292d6e38a79bdc15f4949e23db295bfb
Aditya Sureka <adityasureka92@gmail.com>
2013-08-29 18:11:18 +0530

    my additions

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,18 @@
+add_lunch_combo illusion_crespo-userdebug
+add_lunch_combo illusion_crespo4g-userdebug
 add_lunch_combo illusion_d2att-userdebug
 add_lunch_combo illusion_d2mtr-userdebug
 add_lunch_combo illusion_d2spr-userdebug
 add_lunch_combo illusion_d2tmo-userdebug
 add_lunch_combo illusion_d2usc-userdebug
 add_lunch_combo illusion_d2vzw-userdebug
+add_lunch_combo illusion_flo-userdebug
+add_lunch_combo illusion_galaxysmtd-userdebug
+add_lunch_combo illusion_golden-userdebug
+add_lunch_combo illusion_i777-userdebug
+add_lunch_combo illusion_i9100-userdebug
 add_lunch_combo illusion_i9300-userdebug
+add_lunch_combo illusion_i9305-userdebug
 add_lunch_combo illusion_jfltexx-userdebug
 add_lunch_combo illusion_jflteatt-userdebug
 add_lunch_combo illusion_jfltecan-userdebug
@@ -12,6 +20,16 @@ add_lunch_combo illusion_jfltespr-userdebug
 add_lunch_combo illusion_jfltetmo-userdebug
 add_lunch_combo illusion_jflteusc-userdebug
 add_lunch_combo illusion_jfltevzw-userdebug
+add_lunch_combo illusion_n7000-userdebug
+add_lunch_combo illusion_n7100-userdebug
+add_lunch_combo illusion_m7ul-userdebug
+add_lunch_combo illusion_m7att-userdebug
+add_lunch_combo illusion_m7tmo-userdebug
+add_lunch_combo illusion_m7spr-userdebug
 add_lunch_combo illusion_mako-userdebug
+add_lunch_combo illusion_maguro-userdebug
+add_lunch_combo illusion_t0lte-userdebug
+add_lunch_combo illusion_t0lteatt-userdebug
+add_lunch_combo illusion_toro-userdebug
+add_lunch_combo illusion_toroplus-userdebug
 add_lunch_combo illusion_ville-userdebug
-
